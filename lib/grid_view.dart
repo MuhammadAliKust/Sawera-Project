0ed56a59d220feee5c408973a44907c6b57c1bb5
Muhammad Ali <kustalics@gmail.com>
2025-06-03 09:41:25 +0500

    GridView Builder Done

--- lib/grid_view.dart
+++ lib/grid_view.dart
@@ -9,13 +9,29 @@ class GridViewDemo extends StatelessWidget {
       appBar: AppBar(
         title: Text("Grid View Demo"),
       ),
-      body: GridView.builder(
-          itemCount: 3,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 5, crossAxisSpacing: 30, mainAxisSpacing: 50),
-          itemBuilder: (context, i) {
-            return FlutterLogo();
-          }),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                childAspectRatio: 2.5),
+            itemBuilder: (context, i) {
+              return Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue.withOpacity(0.6),
+                    border: Border.all(color: Colors.blue, width: 3),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Center(
+                  child: Text(
+                    "Urdu",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              );
+            }),
+      ),
     );
   }
 }
